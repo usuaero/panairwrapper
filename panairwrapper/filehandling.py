861bf6ed5667a13ddb5379b3be217f95bd2cc356
Ted Giblette <tedngiblette@gmail.com>
2018-01-30 16:35:37 -0700

    Refactors InputFile class and adds simple test

--- panairwrapper/filehandling.py
+++ panairwrapper/filehandling.py
@@ -4,52 +4,11 @@ The purpose of this module is to handle the formatting details of the Panair
 input and output files. Thus, the user just has to specify the data or ask for
 data and can forget about the details of formatting.
 
-Example
--------
-The following is code for generating an inputfile for a simple Panair case.
-
-import file_handling
-import numpy as np
-
-# Get an InputFile object
-inputfile = file_handling.InputFile()
-
-# Specify the information for each required input block
-inputfile.title("test case", "Ted Giblette")
-inputfile.datacheck(0)
-inputfile.symmetric(1, 0)
-inputfile.mach(1.5)
-inputfile.cases(1)
-inputfile.anglesofattack(0., [-1., 0., 1.])
-inputfile.yawangle(0., [-1., -1., -1.])
-inputfile.printout(0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0)
-
-# The following are just empty arrays. In reality, these would contain
-# coordinates for the points that describe the geometry.
-network_points = np.zeros((3, 11, 3))
-network_points_2 = np.zeros((3, 7, 3))
-inputfile.points(2, 1, ['upper', 'lower'],
-                 [network_points, network_points_2])
-inputfile.trailingwakenetworks(2, 18, 0, ['left', 'right'], ['upper', 'lower'],
-                               [3, 3], [10., 10], [0, 0])
-inputfile.flowfieldproperties(1., 0.)
-
-# Coordinates for points you want to collect off-body data at.
-xyz_offbody_points = np.zeros((10, 3))
-inputfile.xyzcoordinatesofoffbodypoints(10, xyz_offbody_points)
-
-# Generate the inpufile
-inputfile.write_inputfile("./test_input.INP")
-
 Notes
 -----
 Since the functionality of Panair is quite large (much of which is not used
 in common cases), the functionality of this module is being implemented on an
-"as needed" basis. For example, not all input blocks available in Panair have
-been implemented in the InputFile class. Adding something like this is fairly
-straightforward. One would just need to refer to the Panair documentation,
-figure out the appropriate formatting, and then follow the patterns used for
-the other input blocks in the InputFile class to implement the new one.
+"as needed" basis.
 
 This module does not remove the need of the user to understand the information
 that's in the input and output files of the Panair.
@@ -61,7 +20,59 @@ from collections import OrderedDict
 
 
 class InputFile:
-    """  """
+    """Handles the formatting of a Panair input file.
+
+    Data for individual Input Blocks of the inputfile is specified via a member
+    function that corresponds to the input block title. Once all of the
+    required data has been specified in this manner, the inputfile may be
+    generated using the write_inputfile member function.
+
+    Example
+    -------
+    The following is code for generating an inputfile for a simple Panair case.
+
+    import file_handling
+    import numpy as np
+
+    # Get an InputFile object
+    inputfile = file_handling.InputFile()
+
+    # Specify the information for each required input block
+    inputfile.title("test case", "Ted Giblette")
+    inputfile.datacheck(0)
+    inputfile.symmetric(1, 0)
+    inputfile.mach(1.5)
+    inputfile.cases(1)
+    inputfile.anglesofattack(0., [-1., 0., 1.])
+    inputfile.yawangle(0., [-1., -1., -1.])
+    inputfile.printout(0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0)
+
+    # The following are just empty arrays. In reality, these would contain
+    # coordinates for the points that describe the geometry.
+    network_points = np.zeros((3, 11, 3))
+    network_points_2 = np.zeros((3, 7, 3))
+    inputfile.points(2, 1, ['upper', 'lower'],
+                     [network_points, network_points_2])
+    inputfile.trailingwakenetworks(2, 18, 0, ['left', 'right'],
+                                   ['upper', 'lower'],
+                                   [3, 3], [10., 10], [0, 0])
+    inputfile.flowfieldproperties(1., 0.)
+
+    # Coordinates for points you want to collect off-body data at.
+    xyz_offbody_points = np.zeros((10, 3))
+    inputfile.xyzcoordinatesofoffbodypoints(10, xyz_offbody_points)
+
+    # Generate the inpufile
+    inputfile.write_inputfile("./test_input.INP")
+
+    Notes
+    -----
+    Not all input blocks available in Panair have been implemented in the
+    InputFile class. Adding something like this is fairly straightforward
+    however. One would just need to refer to the Panair documentation, figure
+    out the appropriate formatting, and then follow the patterns used for
+    the other input blocks this class to implement the new one.
+    """
     def __init__(self):
         self._input_dict = OrderedDict()
 
@@ -82,75 +93,84 @@ class InputFile:
 
     @staticmethod
     def _format_coord(coordinates):
-        return '{0[0]:10f}{0[1]:10f}{0[2]:10f}'.format(coordinates)
+        return '{0[0]:<10f}{0[1]:<10f}{0[2]:<10f}'.format(coordinates)
+
+    def _format_header(self, labels):
+        header = ""
+        for i, l in enumerate(labels):
+            if i == 0:
+                header += self._format_str_opt("="+l)
+            else:
+                header += self._format_str_opt(l)
+        header += "\n"
+
+        return header
+
+    def _format_inputline(self, values):
+        inputs = ""
+        for v in values:
+            inputs += self._format_opt(v)
+        inputs += "\n"
+
+        return inputs
 
     def title(self, title, info):
         self._input_dict["TITLE"] = title+"\n"+info+"\n"
 
     def datacheck(self, ndtchk):
-        header = "=ndtchk\n"
-        option = self._format_opt(ndtchk)+"\n"
+        # header = "=ndtchk\n"
+        header = self._format_header(["ndtchk"])
+        option = self._format_inputline([ndtchk])
         self._input_dict["DATACHECK"] = header+option
 
     def symmetric(self, xzpln, xypln):
-        header = "=xzpln    xypln\n"
-        options = self._format_opt(xzpln)+self._format_opt(xypln)+"\n"
+        header = self._format_header(["xzpln", "xypln"])
+        options = self._format_inputline([xzpln, xypln])
         self._input_dict["SYMMETRIC"] = header+options
 
     def mach(self, amach):
-        header = "=amach\n"
-        value = self._format_opt(amach)+"\n"
+        header = self._format_header(["amach"])
+        value = self._format_inputline([amach])
         self._input_dict["MACH NUMBER"] = header+value
 
     def cases(self, nacase):
-        header = "=nacase\n"
-        option = self._format_opt(nacase)+"\n"
+        header = self._format_header(["nacase"])
+        option = self._format_inputline([nacase])
         self._input_dict["CASES"] = header+option
 
     def anglesofattack(self, alpc, alphas):
-        header1 = "=alpc\n"
-        input1 = self._format_opt(alpc)+"\n"
-        header2 = "="
-        input2 = ""
-
+        header1 = self._format_header(["alpc"])
+        input1 = self._format_inputline([alpc])
+        # generate headers for alphas based on number
+        headers_2 = []
         for i, a in enumerate(alphas):
-            header2 += "alpha("+str(i)+")  "
-            input2 += self._format_opt(a)
-
-        header2 += "\n"
-        input2 += "\n"
+            headers_2.append("alpha("+str(i)+")")
+        header2 = self._format_header(headers_2)
+        input2 = self._format_inputline(alphas)
 
         aoa_input = header1+input1+header2+input2
 
         self._input_dict["ANGLES OF ATTACK"] = aoa_input
 
     def yawangle(self, betc, betas):
-        header1 = "=betc\n"
-        input1 = self._format_opt(betc)+"\n"
-        header2 = "="
-        input2 = ""
-
+        header1 = self._format_header(["betc"])
+        input1 = self._format_inputline([betc])
+        # generate headers for betas based on number
+        headers_2 = []
         for i, b in enumerate(betas):
-            header2 += "beta("+str(i)+")   "
-            input2 += self._format_opt(b)
-
-        header2 += "\n"
-        input2 += "\n"
+            headers_2.append("beta("+str(i)+")")
+        header2 = self._format_header(headers_2)
+        input2 = self._format_inputline(betas)
 
         beta_input = header1+input1+header2+input2
 
         self._input_dict["YAW ANGLE"] = beta_input
 
     def referencedata(self, xref, yref, zref, sref, bref, cref, dref):
-        header1 = "=xref     yref      zref\n"
-        input1 = self._format_opt(xref)
-        input1 += self._format_opt(yref)
-        input1 += self._format_opt(zref)+"\n"
-        header2 = "=sref     bref      cref      dref\n"
-        input2 = self._format_opt(sref)
-        input2 += self._format_opt(bref)
-        input2 += self._format_opt(cref)
-        input2 += self._format_opt(dref)+"\n"
+        header1 = self._format_header(["xref", "yref", "zref"])
+        input1 = self._format_inputline([xref, yref, zref])
+        header2 = self._format_header(["sref", "bref", "cref", "dref"])
+        input2 = self._format_inputline([sref, bref, cref, dref])
 
         ref_input = header1+input1+header2+input2
 
@@ -158,24 +178,24 @@ class InputFile:
 
     def printout(self, isings, igeomp, isingp, icontp, ibconp, iedgep,
                  ipraic, nexdgn, ioutpr, ifmcpr, icostp):
-        header1 = "=isings   igeomp    isingp    icontp    ibconp    iedgep\n"
-        input1 = self._format_opt(isings)+self._format_opt(igeomp)
-        input1 += self._format_opt(isingp)+self._format_opt(icontp)
-        input1 += self._format_opt(ibconp)+self._format_opt(iedgep)+"\n"
-        header2 = "=ipraic   nexdgn    ioutpr    ifmcpr    icostp\n"
-        input2 = self._format_opt(ipraic)+self._format_opt(nexdgn)
-        input2 += self._format_opt(ioutpr)+self._format_opt(ifmcpr)
-        input2 += self._format_opt(icostp)+"\n"
+        header1 = self._format_header(["isings", "igeomp", "isingp",
+                                       "icontp", "ibconp", "iedgep"])
+        input1 = self._format_inputline([isings, igeomp, isingp,
+                                         icontp, ibconp, iedgep])
+        header2 = self._format_header(["ipraic", "nexdgn", "ioutpr",
+                                       "ifmcpr", "icostp"])
+        input2 = self._format_inputline([ipraic, nexdgn, ioutpr,
+                                         ifmcpr, icostp])
 
         printout = header1+input1+header2+input2
 
         self._input_dict["PRINTOUT CONTROL"] = printout
 
     def points(self, kn, kt, netnames, netpoints):
-        header1 = "=kn\n"
-        input1 = self._format_opt(kn)+"\n"
-        header2 = "=kt\n"
-        input2 = self._format_opt(kt)+"\n"
+        header1 = self._format_header(["kn"])
+        input1 = self._format_inputline([kn])
+        header2 = self._format_header(["kt"])
+        input2 = self._format_inputline([kt])
         points_input = header1+input1+header2+input2
         for i in range(int(kn)):
             points_input += self._gen_network_inp(netnames[i], netpoints[i])
@@ -186,7 +206,7 @@ class InputFile:
         header = ("=nm       nn                                             " +
                   "    " + netname + "\n")
         nn, nm = points.shape[:2]
-        values = self._format_opt(nm)+self._format_opt(nn)+"\n"
+        values = self._format_inputline([nm, nn])
         network_input = header+values
         for i in range(int(nn)):
             for j in range(int(nm)):
@@ -202,10 +222,10 @@ class InputFile:
 
     def trailingwakenetworks(self, kn, kt, matchw, netnames, inat,
                              insd, xwake, twake):
-        header1 = "=kn\n"
-        input1 = self._format_opt(kn)+"\n"
-        header2 = "=kt       matchw\n"
-        input2 = self._format_opt(kt)+self._format_opt(matchw)+"\n"
+        header1 = self._format_header(["kn"])
+        input1 = self._format_inputline([kn])
+        header2 = self._format_header(["kt", "matchw"])
+        input2 = self._format_inputline([kt, matchw])
         wake_input = header1+input1+header2+input2
         for i in range(int(kn)):
             wake_input += self._gen_wake_inp(netnames[i], inat[i], insd[i],
@@ -222,15 +242,16 @@ class InputFile:
         return header+values
 
     def flowfieldproperties(self, nflowv, tpoff):
-        header = "=nflowv   tpoff     \n"
-        values = self._format_opt(nflowv)+self._format_opt(tpoff)+"\n"
+        header = self._format_header(["nflowv", "tpoff"])
+        values = self._format_inputline([nflowv, tpoff])
 
         self._input_dict["FLOW-FIELD PROPERTIES"] = header+values
 
     def xyzcoordinatesofoffbodypoints(self, isk1, points):
-        header1 = "=isk1\n"
-        input1 = self._format_opt(isk1)+"\n"
-        header2 = "xof       yof       zof       xof       yof       zof\n"
+        header1 = self._format_header(["isk1"])
+        input1 = self._format_inputline([isk1])
+        header2 = self._format_header(["xof", "yof", "zof",
+                                       "xof", "yof", "zof"])
         offbody_input = header1+input1+header2
         for i in range(int(isk1)):
             offbody_input += self._format_coord(points[i])
---
+++ test/test_filehandler.py
@@ -0,0 +1,42 @@
+"""Tests the file handling model."""
+# pylint: disable=redefined-outer-name
+
+import pytest
+import filecmp
+import numpy as np
+import panairwrapper.filehandling as fh
+
+
+TESTFILE_DIR = "./test/testfiles/"
+
+
+def test_inputfile():
+
+    inputfile = fh.InputFile()
+
+    inputfile.title("test case", "Ted Giblette")
+    inputfile.datacheck(0)
+    inputfile.symmetric(1, 0)
+    inputfile.mach(1.5)
+    inputfile.cases(1)
+    inputfile.anglesofattack(0., [-1., 0., 1.])
+    inputfile.yawangle(0., [-1., -1., -1.])
+    inputfile.printout(0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0)
+
+    network_points = np.zeros((3, 11, 3))
+    network_points_2 = np.zeros((3, 7, 3))
+    inputfile.points(2, 1, ['upper', 'lower'],
+                     [network_points, network_points_2])
+    inputfile.trailingwakenetworks(2, 18, 0, ['left', 'right'],
+                                   ['upper', 'lower'],
+                                   [3, 3], [10., 10], [0, 0])
+    inputfile.flowfieldproperties(1., 0.)
+    xyz_offbody_points = np.zeros((10, 3))
+    inputfile.xyzcoordinatesofoffbodypoints(10, xyz_offbody_points)
+
+    newfilename = TESTFILE_DIR+"test_input.INP"
+    reffilename = TESTFILE_DIR+"inputfile.REF"
+
+    inputfile.write_inputfile(newfilename)
+
+    assert filecmp.cmp(newfilename, reffilename)
