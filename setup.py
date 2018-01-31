de1794f00324968cc8d488136ed1ea3479c8ca8c
Ted Giblette <tedngiblette@gmail.com>
2018-01-31 15:56:58 -0700

    Adds basic setup.py file to facilitate local usage

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""panairwrapper: A wrapper for the Panair/A502 Fortran code."""
+
+from setuptools import setup
+
+setup(name='panairwrapper',
+      version='0.0',
+      description='A wrapper for the Panair/A502 Fortran code.',
+      url='NA',
+      author='usuaero',
+      author_email='doug.hunsaker@usu.edu',
+      license='MIT',
+      packages=['panairwrapper'],
+      zip_safe=False)
