14a63c571e422893e84fd6e3bdc7c0f98b1122b9
Ted Giblette <tedngiblette@gmail.com>
2018-08-06 14:38:24 -0700

    Fixes test for windows

--- test/test_panairwrapper.py
+++ test/test_panairwrapper.py
@@ -1,17 +1,24 @@
 """ """
 import pytest
 import os
+import platform
 
 import panairwrapper
 
 TESTFILE_DIR = os.path.join(os.path.dirname(__file__), 'testfiles')
 
 
+PANAIR_EXE = 'panair'
+if platform.system() == 'Windows':
+    PANAIR_EXE = 'panair.exe'
+
 @pytest.fixture
 def empty_case():
     # setup
+
     """Construct a PanairWrapper for an empty case with no networks."""
-    panair_case = panairwrapper.PanairWrapper("empty_case", TESTFILE_DIR)
+    panair_case = panairwrapper.PanairWrapper("empty_case", TESTFILE_DIR,
+                                              exe=PANAIR_EXE)
 
     yield panair_case
 
@@ -23,6 +30,6 @@ def test_generate_dir(empty_case):
     empty_case._generate_dir(True)
 
     assert os.path.isdir(os.path.join(TESTFILE_DIR, "panair_files"))
-    assert os.path.isfile(os.path.join(TESTFILE_DIR, "panair_files", "panair"))
+    assert os.path.isfile(os.path.join(TESTFILE_DIR, "panair_files", PANAIR_EXE))
 
 
